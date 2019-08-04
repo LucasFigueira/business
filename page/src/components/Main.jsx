dc632da8cd6356b2a50af75f9dda12772dedd450
LucasFigueira <lfgon7@gmail.com>
2019-08-04 19:51:23 -0300

    comentário

--- page/src/components/Main.jsx
+++ page/src/components/Main.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer'
 export default props =>
     <React.Fragment>
         <Header {...props} />
-        <main className="container"> 
+        <main className="container-fluid"> 
                  {props.children} 
         </main> 
     </React.Fragment>
